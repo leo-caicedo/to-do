61e57086937e54f90996a4afb8d1758493cdf249
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-05-25 13:51:30 -0500

    task model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,22 @@
+# Django
 from django.db import models
 
-# Create your models here.
+# Models
+from django.contrib.auth.models import User
+
+
+class Task(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=30)
+    description = models.TextField(max_length=255, blank=True)
+    complete = models.BooleanField(default=False)
+
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['complete']
